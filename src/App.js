2f7fa6e9c4e7058b4285b6bd7201eac245cd558d
Meesala <deepa.meesala@accenture.com>
2019-11-05 17:42:43 +0530

    updated app with shop page, collection preview ad collection item components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-const Hatspage= () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
-        <Route exact path='/' component={HomePage}/>
-        <Route path='/shop/hats' component={Hatspage}/>
+      <Switch>
+        <Route exact path= '/' component= {HomePage}/>
+        <Route path= '/shop' component= {ShopPage}/>
+      </Switch>
     </div>
   );
 }
+
 
 export default App;
