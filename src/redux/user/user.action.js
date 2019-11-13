84020b0fa3581874ca8a5ff726264c2510a4e3f9
Meesala <deepa.meesala@accenture.com>
2019-11-13 13:09:06 +0530

    added user redirect to homepage when signed in

--- src/redux/user/user.action.js
+++ src/redux/user/user.action.js
@@ -1,4 +1,7 @@
+import { UserActionTypes } from './user.types';
+import userReducer from './user.reducer';
+
 export const setCurrentUser = user => ({
-    type: 'SET_CURRENT_USER',
+    type: UserActionTypes.SET_CURRENT_USER,
     payload: user
 })
